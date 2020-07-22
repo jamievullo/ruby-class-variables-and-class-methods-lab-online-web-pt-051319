a4113b9515861ca65c36d1eb5f32d639df213d89
Jamie Vullo <jvullo@protonmail.com>
2020-07-22 13:17:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,42 @@
-class Song 
 
-  attr_accessor :name, :artist, :genre 
+class Song 
   
-  @@count = 0 
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
   @@genres = []
   @@artists = []
   
   def initialize(name, artist, genre)
-    @@count += 1 
-    @name = name 
+    @name = name
     @artist = artist
     @genre = genre
-    @@artists << artist 
-    @@genres << genre 
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    
   end
   
-  def self.count 
-    @@count 
+  def self.count
+    return @@count
   end
   
   def self.genres
-    @@genres.uniq
+      return @@genres.uniq 
   end
   
   def self.artists
-    @@artists.uniq
+    return @@artists.uniq
   end
   
   def self.genre_count
-    genre_count = Hash.new(0)
-    @@genres.each {|genre| genre_count[genre] += 1}
-    genre_count
+    @@genres.each do |g|
+      
+    end
   end
-
-  def self.artist_count
-    artist_count = Hash.new(0)
-    @@artists.each {|artist| artist_count[artist] += 1}
-    artist_count
+  
+  def artist_count
+    
   end
-
- 
-end
+  
+end 
